e808acd6a12055f27707c198f7a2b36d8f564024
Pål Simen Ruud <31000369+palsimen@users.noreply.github.com>
2017-11-13 19:37:42 +0100

    Added --debug and --nodisplay options

--- NetatmoWeatherDisplay.py
+++ NetatmoWeatherDisplay.py
@@ -1,35 +1,49 @@
 import argparse
 import logging
-import pprint
+import time
 
 from NetatmoAccess import NetatmoAccess
 
-name = "NetatmoWeatherDisplay.py"
+NAME = "NetatmoWeatherDisplay.py"
+UPDATE_INTERVAL = 5
 
-pp = pprint.PrettyPrinter()
+log = logging.getLogger(NAME)
 
-logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger(name)
-
-parser = argparse.ArgumentParser(description=name)
+parser = argparse.ArgumentParser(description=NAME)
 parser.add_argument('--username',      help='username',      required=True)
 parser.add_argument('--password',      help='password',      required=True)
 parser.add_argument('--client_id',     help='client_id',     required=True)
 parser.add_argument('--client_secret', help='client_secret', required=True)
+parser.add_argument('--nodisplay',     help='run system without zero seg display', action='store_true')
+parser.add_argument('--debug',         help='enable debug logging')
 
 args = parser.parse_args()
 
-
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.ERROR)
 
 netatmo = NetatmoAccess(username=args.username,
-                   password=args.password,
-                   client_id=args.client_id,
-                   client_secret=args.client_secret)  
-netatmo.update()
-try:
-    data = netatmo.get('Outdoor module')
-    pp.pprint(data)
-    data = netatmo.get('Outdoo module')
-    pp.pprint(data)
-except KeyError, error:
-    print 'Could not find module name. Error:', error
+                        password=args.password,
+                        client_id=args.client_id,
+                        client_secret=args.client_secret)  
+
+while True:
+    netatmo.update()
+    try:
+        indoor = netatmo.get('Indoor')
+        outdoor = netatmo.get('Outdoor module')
+
+        # Display in terminal
+        if args.nodisplay:
+            print "Inne: " + str(indoor['Temperature'])
+            print "Ute: " + str(outdoor['Temperature'])
+        # Display on zero seg
+        else:
+            print "Zero seg"
+
+    except KeyError, error:
+        print 'Could not find module name. Error:', error
+
+    time.sleep(UPDATE_INTERVAL)
